eaa38e95d3b631b0fd9126f3cd4d804de906a0cc
TheTiscks <skretov006@gmail.com>
2026-05-26 15:33:18 +0300

    SOCK_STREAM-TCP Network module

---
+++ project/common/include/Network.h
@@ -0,0 +1,38 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <string>
+#include <stdexcept>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+class Socket {
+public:
+	Socket();
+	~Socket();
+	void close();
+
+	// Серверные методы
+	void bind(int port);
+	void listen(int backlog = 5);
+	int accept();
+
+	// Клиентские методы
+	void connect(const std::string& host, int port);
+
+	// Отправка/получение строк
+	void send(const std::string& msg);
+	std::string recv();
+
+	// Для передачи дескриптора клиенту
+	int getFd() const { return fd_; }
+	void setFd(int fd) { fd_ = fd; }
+
+private:
+	int fd_;
+};
+
+#endif
